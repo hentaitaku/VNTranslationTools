a5d190b72168a8ac0ec2961b60f3f1e9fd094380
lifegpc <root@lifegpc.com>
2025-04-18 17:16:53 +0800

    BugFix

--- VNTextPatch.Shared/GlobalVar.cs
+++ VNTextPatch.Shared/GlobalVar.cs
@@ -5,5 +5,5 @@ public static class GlobalVariables
     public static bool ReadUtf8 = false;
     public static bool WriteUtf8 = false;
     public static bool NoWrap = false;
-    public static HashSet<string> jisStrings;
+    public static HashSet<string> jisStrings = new HashSet<string>();
 } 
